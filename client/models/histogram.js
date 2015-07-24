522d79945cccb57a7b53cfa2c235d08a27ca82f4
Jisk Attema <j.attema@esciencecenter.nl>
2015-07-24 16:17:35 +0200

    remove unused property

--- client/models/histogram.js
+++ client/models/histogram.js
@@ -3,7 +3,6 @@ var widgetModel = require('./widget');
 module.exports = widgetModel.extend({
     props: {
         missing: ['number', false],
-        chart: ['any', false ],
         filtermin: ['number', false],
         filtermax: ['number', false],
     }

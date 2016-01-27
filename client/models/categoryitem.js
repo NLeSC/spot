c7bed0a64e068be51a905f374497934ed6524fb0
Jisk Attema <j.attema@esciencecenter.nl>
2016-01-27 21:20:55 +0100

    Add documentation for categorial facets

--- client/models/categoryitem.js
+++ client/models/categoryitem.js
@@ -1,10 +1,12 @@
 var AmpersandModel = require('ampersand-model');
 
+// Data structure for working with categorial data
+// it helps with mapping a dataitem (category) on a group
 module.exports = AmpersandModel.extend({
     props: {
-        category: ['any', true, ''],
-        count: ['number', true, 0],
-        group: ['any', true, ''],
+        category: ['any', true, ''], // string format of regexp to match data against
+        count: ['number', true, 0],  // number of items in this category
+        group: ['any', true, ''],    // name of the group this is mapped to
     },
     derived: {
         category_regexp: {

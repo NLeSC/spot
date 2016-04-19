7f1ba1836bdf75577a53ea6606e8466f8e5e4727
Jisk Attema <j.attema@esciencecenter.nl>
2016-04-19 08:09:57 +0200

    Start with different dataset

--- client/app.js
+++ client/app.js
@@ -6,6 +6,7 @@ var domReady = require('domready');
 var widgetFactory = require('./widget_factory');
 var crossfilter = require('crossfilter');
 var d3 = require('d3');
+var util = require('./util');
 
 // FIXME: i can't get the componentHandler exported via browserify-shim
 // the require below will add it as window.componentHandler
@@ -26,13 +27,14 @@ app.extend({
     init: function() {
 
         // Load the actual data, and add it to the crossfilter when ready
-        d3.json('./data/data.json', function (error,json) {
-
+        d3.json('./data/software.json', function (error,json) {
+    
             window.app.crossfilter.add(json);
+            util.scanData();
 
-            d3.json('./data/session.json', function (error,json) {
-                window.app.me.set(json);
-            }); 
+            //d3.json('./data/session.json', function (error,json) {
+            //    window.app.me.set(json);
+            //}); 
 
         });
 

62d4a999d28f6004aa0614ee5e88f0fb407266ca
fdiblen <f.diblen@esciencecenter.nl>
2016-10-03 11:01:03 +0200

    update for 3d

--- client/views/widget-frame.js
+++ client/views/widget-frame.js
@@ -5,7 +5,6 @@ var Partition = require('../models/partition');
 var PartitionButtonView = require('./partition-button');
 var interact = require('interact.js');
 
-
 function facetFromEvent (view, ev) {
   var filter = view.model.filter;
   var dataset = filter.collection.parent;
@@ -114,59 +113,56 @@ module.exports = View.extend({
     partitions.add(partition);
   },
 
-
-/////////////////////////////////////////////
+// ///////////////////////////////////////////
 
   setupDragDrop: function (interact) {
-      'use strict';
-      var transformProp;
-      interact.maxInteractions(Infinity);
+    'use strict';
+    var transformProp;
+    interact.maxInteractions(Infinity);
 
       // setup draggable elements.
-      interact('.js-drag')
+    interact('.js-drag')
           .draggable({ max: Infinity })
           .on('dragstart', function (event) {
-              event.interaction.x = parseInt(event.target.getAttribute('data-x'), 10) || 0;
-              event.interaction.y = parseInt(event.target.getAttribute('data-y'), 10) || 0;
+            event.interaction.x = parseInt(event.target.getAttribute('data-x'), 10) || 0;
+            event.interaction.y = parseInt(event.target.getAttribute('data-y'), 10) || 0;
           })
           .on('dragmove', function (event) {
-              event.interaction.x += event.dx;
-              event.interaction.y += event.dy;
+            event.interaction.x += event.dx;
+            event.interaction.y += event.dy;
 
-              if (transformProp) {
-                  event.target.style[transformProp] =
+            if (transformProp) {
+              event.target.style[transformProp] =
                       'translate(' + event.interaction.x + 'px, ' + event.interaction.y + 'px)';
-              }
-              else {
-                  event.target.style.left = event.interaction.x + 'px';
-                  event.target.style.top  = event.interaction.y + 'px';
-              }
+            }
+            else {
+              event.target.style.left = event.interaction.x + 'px';
+              event.target.style.top = event.interaction.y + 'px';
+            }
           })
           .on('dragend', function (event) {
-              event.target.setAttribute('data-x', event.interaction.x);
-              event.target.setAttribute('data-y', event.interaction.y);
+            event.target.setAttribute('data-x', event.interaction.x);
+            event.target.setAttribute('data-y', event.interaction.y);
           });
 
       // setup drop areas.
-      var dragFacet1 = this.queryByHook('dragFacet1');
-      var dragFacet2 = this.queryByHook('dragFacet2');
-      var dropFacet = this.queryByHook('dropfacet');
+    var dragFacet1 = this.queryByHook('dragFacet1');
+    var dragFacet2 = this.queryByHook('dragFacet2');
+    var dropFacet = this.queryByHook('dropfacet');
     //   var drop2 = this.queryByHook('drop2');
     //   var drop3 = this.queryByHook('drop3');
 
       // dropzone #1 accepts draggable #1
-      //setupDropzone('#drop1', '#drag1');
-      //setupDropzone(this.queryByHook('drop1'), this.queryByHook('drag1'));
-
+      // setupDropzone('#drop1', '#drag1');
+      // setupDropzone(this.queryByHook('drop1'), this.queryByHook('drag1'));
 
       // dropzone #2 accepts draggable #1 and #2
-      //setupDropzone('#drop2', '#drag1, #drag2');
+      // setupDropzone('#drop2', '#drag1, #drag2');
       // every dropzone accepts draggable #3
-      //setupDropzone('.js-drop', '#drag3');
+      // setupDropzone('.js-drop', '#drag3');
 
-
-      setupDropzone('.js-drop', dragFacet1);
-      setupDropzone('.js-drop', dragFacet2);
+    setupDropzone('.js-drop', dragFacet1);
+    setupDropzone('.js-drop', dragFacet2);
 
       /**
        * Setup a given element as a dropzone.
@@ -174,89 +170,84 @@ module.exports = View.extend({
        * @param {HTMLElement|String} el
        * @param {String} accept
        */
-      function setupDropzone(el, accept) {
-          interact(el)
+    function setupDropzone (el, accept) {
+      interact(el)
               .dropzone({
-                  accept: accept,
-                  ondropactivate: function (event) {
-                      addClass(event.relatedTarget, '-drop-possible');
-                  },
-                  ondropdeactivate: function (event) {
-                      removeClass(event.relatedTarget, '-drop-possible');
-                  }
+                accept: accept,
+                ondropactivate: function (event) {
+                  addClass(event.relatedTarget, '-drop-possible');
+                },
+                ondropdeactivate: function (event) {
+                  removeClass(event.relatedTarget, '-drop-possible');
+                }
               })
               .on('dropactivate', function (event) {
-                  var active = event.target.getAttribute('active')|0;
+                var active = event.target.getAttribute('active') | 0;
 
                   // change style if it was previously not active
-                  if (active === 0) {
-                      addClass(event.target, '-drop-possible');
-                      event.target.textContent = 'Drop me here!';
-                  }
+                if (active === 0) {
+                  addClass(event.target, '-drop-possible');
+                  event.target.textContent = 'Drop me here!';
+                }
 
-                  event.target.setAttribute('active', active + 1);
+                event.target.setAttribute('active', active + 1);
               })
               .on('dropdeactivate', function (event) {
-                  var active = event.target.getAttribute('active')|0;
+                var active = event.target.getAttribute('active') | 0;
 
                   // change style if it was previously active
                   // but will no longer be active
-                  if (active === 1) {
-                      removeClass(event.target, '-drop-possible');
-                      event.target.textContent = 'Dropzone';
-                  }
+                if (active === 1) {
+                  removeClass(event.target, '-drop-possible');
+                  event.target.textContent = 'Dropzone';
+                }
 
-                  event.target.setAttribute('active', active - 1);
+                event.target.setAttribute('active', active - 1);
               })
               .on('dragenter', function (event) {
-                  addClass(event.target, '-drop-over');
-                  event.relatedTarget.textContent = 'I\'m in';
+                addClass(event.target, '-drop-over');
+                event.relatedTarget.textContent = 'I\'m in';
               })
               .on('dragleave', function (event) {
-                  removeClass(event.target, '-drop-over');
-                  event.relatedTarget.textContent = 'Drag me…';
+                removeClass(event.target, '-drop-over');
+                event.relatedTarget.textContent = 'Drag me…';
               })
               .on('drop', function (event) {
-                  removeClass(event.target, '-drop-over');
-                  event.relatedTarget.textContent = 'Dropped';
+                removeClass(event.target, '-drop-over');
+                event.relatedTarget.textContent = 'Dropped';
               });
+    }
+
+    function addClass (element, className) {
+      if (element.classList) {
+        return element.classList.add(className);
       }
-
-      function addClass (element, className) {
-          if (element.classList) {
-              return element.classList.add(className);
-          }
-          else {
-              element.className += ' ' + className;
-          }
+      else {
+        element.className += ' ' + className;
       }
+    }
 
-      function removeClass (element, className) {
-          if (element.classList) {
-              return element.classList.remove(className);
-          }
-          else {
-              element.className = element.className.replace(new RegExp(className + ' *', 'g'), '');
-          }
+    function removeClass (element, className) {
+      if (element.classList) {
+        return element.classList.remove(className);
       }
+      else {
+        element.className = element.className.replace(new RegExp(className + ' *', 'g'), '');
+      }
+    }
 
-      interact(document).on('ready', function () {
-          transformProp = 'transform' in document.body.style
-              ? 'transform': 'webkitTransform' in document.body.style
-              ? 'webkitTransform': 'mozTransform' in document.body.style
-              ? 'mozTransform': 'oTransform' in document.body.style
-              ? 'oTransform': 'msTransform' in document.body.style
-              ? 'msTransform': null;
-      });
-      //this.window.interact;
-
+    interact(document).on('ready', function () {
+      transformProp = 'transform' in document.body.style
+              ? 'transform' : 'webkitTransform' in document.body.style
+              ? 'webkitTransform' : 'mozTransform' in document.body.style
+              ? 'mozTransform' : 'oTransform' in document.body.style
+              ? 'oTransform' : 'msTransform' in document.body.style
+              ? 'msTransform' : null;
+    });
+      // this.window.interact;
   }
 
-
-
-  /////////////////////////////////////////////
-
-
+  // ///////////////////////////////////////////
 
   ,
   closeWidget: function () {
@@ -271,7 +262,7 @@ module.exports = View.extend({
     this.model.filter.title = this.queryByHook('title-input').value;
   },
   render: function () {
-    //this.setupDragDrop(interact);
+    // this.setupDragDrop(interact);
     this.renderWithTemplate(this);
     this.renderCollection(this.model.filter.partitions, PartitionButtonView, this.queryByHook('dropzone'));
     return this;

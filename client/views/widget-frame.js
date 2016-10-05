013ddc190030311fb9f6258e17730f4b2e083a1b
fdiblen <f.diblen@esciencecenter.nl>
2016-10-05 12:58:20 +0200

    removed react.js stuff from this branch Made some makeup for 3D scatter chart and is functional now.

--- client/views/widget-frame.js
+++ client/views/widget-frame.js
@@ -3,7 +3,6 @@ var templates = require('../templates');
 var app = require('ampersand-app');
 var Partition = require('../models/partition');
 var PartitionButtonView = require('./partition-button');
-var interact = require('interact.js');
 
 function facetFromEvent (view, ev) {
   var filter = view.model.filter;
@@ -113,143 +112,6 @@ module.exports = View.extend({
     partitions.add(partition);
   },
 
-// ///////////////////////////////////////////
-
-  setupDragDrop: function (interact) {
-    'use strict';
-    var transformProp;
-    interact.maxInteractions(Infinity);
-
-      // setup draggable elements.
-    interact('.js-drag')
-          .draggable({ max: Infinity })
-          .on('dragstart', function (event) {
-            event.interaction.x = parseInt(event.target.getAttribute('data-x'), 10) || 0;
-            event.interaction.y = parseInt(event.target.getAttribute('data-y'), 10) || 0;
-          })
-          .on('dragmove', function (event) {
-            event.interaction.x += event.dx;
-            event.interaction.y += event.dy;
-
-            if (transformProp) {
-              event.target.style[transformProp] =
-                      'translate(' + event.interaction.x + 'px, ' + event.interaction.y + 'px)';
-            }
-            else {
-              event.target.style.left = event.interaction.x + 'px';
-              event.target.style.top = event.interaction.y + 'px';
-            }
-          })
-          .on('dragend', function (event) {
-            event.target.setAttribute('data-x', event.interaction.x);
-            event.target.setAttribute('data-y', event.interaction.y);
-          });
-
-      // setup drop areas.
-    var dragFacet1 = this.queryByHook('dragFacet1');
-    var dragFacet2 = this.queryByHook('dragFacet2');
-    var dropFacet = this.queryByHook('dropfacet');
-    //   var drop2 = this.queryByHook('drop2');
-    //   var drop3 = this.queryByHook('drop3');
-
-      // dropzone #1 accepts draggable #1
-      // setupDropzone('#drop1', '#drag1');
-      // setupDropzone(this.queryByHook('drop1'), this.queryByHook('drag1'));
-
-      // dropzone #2 accepts draggable #1 and #2
-      // setupDropzone('#drop2', '#drag1, #drag2');
-      // every dropzone accepts draggable #3
-      // setupDropzone('.js-drop', '#drag3');
-
-    setupDropzone('.js-drop', dragFacet1);
-    setupDropzone('.js-drop', dragFacet2);
-
-      /**
-       * Setup a given element as a dropzone.
-       *
-       * @param {HTMLElement|String} el
-       * @param {String} accept
-       */
-    function setupDropzone (el, accept) {
-      interact(el)
-              .dropzone({
-                accept: accept,
-                ondropactivate: function (event) {
-                  addClass(event.relatedTarget, '-drop-possible');
-                },
-                ondropdeactivate: function (event) {
-                  removeClass(event.relatedTarget, '-drop-possible');
-                }
-              })
-              .on('dropactivate', function (event) {
-                var active = event.target.getAttribute('active') | 0;
-
-                  // change style if it was previously not active
-                if (active === 0) {
-                  addClass(event.target, '-drop-possible');
-                  event.target.textContent = 'Drop me here!';
-                }
-
-                event.target.setAttribute('active', active + 1);
-              })
-              .on('dropdeactivate', function (event) {
-                var active = event.target.getAttribute('active') | 0;
-
-                  // change style if it was previously active
-                  // but will no longer be active
-                if (active === 1) {
-                  removeClass(event.target, '-drop-possible');
-                  event.target.textContent = 'Dropzone';
-                }
-
-                event.target.setAttribute('active', active - 1);
-              })
-              .on('dragenter', function (event) {
-                addClass(event.target, '-drop-over');
-                event.relatedTarget.textContent = 'I\'m in';
-              })
-              .on('dragleave', function (event) {
-                removeClass(event.target, '-drop-over');
-                event.relatedTarget.textContent = 'Drag me…';
-              })
-              .on('drop', function (event) {
-                removeClass(event.target, '-drop-over');
-                event.relatedTarget.textContent = 'Dropped';
-              });
-    }
-
-    function addClass (element, className) {
-      if (element.classList) {
-        return element.classList.add(className);
-      }
-      else {
-        element.className += ' ' + className;
-      }
-    }
-
-    function removeClass (element, className) {
-      if (element.classList) {
-        return element.classList.remove(className);
-      }
-      else {
-        element.className = element.className.replace(new RegExp(className + ' *', 'g'), '');
-      }
-    }
-
-    interact(document).on('ready', function () {
-      transformProp = 'transform' in document.body.style
-              ? 'transform' : 'webkitTransform' in document.body.style
-              ? 'webkitTransform' : 'mozTransform' in document.body.style
-              ? 'mozTransform' : 'oTransform' in document.body.style
-              ? 'oTransform' : 'msTransform' in document.body.style
-              ? 'msTransform' : null;
-    });
-      // this.window.interact;
-  }
-
-  // ///////////////////////////////////////////
-
-  ,
   closeWidget: function () {
     // Remove the filter from the dataset
     var filters = this.model.filter.collection;
@@ -269,7 +131,6 @@ module.exports = View.extend({
   },
   renderContent: function () {
     // Propagate to subview
-    this.setupDragDrop(interact);
     this.widget.renderContent();
   },
   subviews: {
--- client/views/widget-plotly3d.js
+++ client/views/widget-plotly3d.js
@@ -2,41 +2,74 @@ var AmpersandView = require('ampersand-view');
 var templates = require('../templates');
 var Plotly = require('plotly.js');
 
-
 function initChart (view) {
-  var filter = view.model.filter;
-
-  // Configure plot
   view._config = view.model.plotlyConfig();
   var options = view._config.options;
-  var layout = view.model.plotLayout();
-  var chartData = view._config.data;
+  var layout = view._config.layout;
   var graphDiv = view.queryByHook('chart-area-plotly');
 
-
-  // console.log('-- filter --\n', filter);
-  // console.log('-- view._config --\n', view._config);
-  // console.log('-- options --\n', options);
-  // console.log('-- layout --\n', layout);
-  // console.log('-- chartData --\n', chartData);
-  // console.log('-- graphDiv --\n', graphDiv);
-
-
-//  view._plotly = Plotly.newPlot(graphDiv, mydata, mylayout, options);
-
-
-//  view._plotly = Plotly.newPlot(graphDiv, chartData, layout, options);
-  view._plotly = 0;
+  view._plotly = Plotly.newPlot(graphDiv, [], layout, options);
 
   // In callbacks on the chart we will need the view, so store a reference
   view._plotly._Ampersandview = view;
 }
 
+function updateScatter (view) {
+  var filter = view.model.filter;
+  var chartData = view._config.data;
+
+  var primary = filter.partitions.get('1', 'rank');
+  var secondary = filter.partitions.get('2', 'rank');
+  var tertiary = filter.partitions.get('3', 'rank');
+
+  var xgroups = primary.groups;
+  var ygroups = secondary.groups;
+  var zgroups = tertiary.groups;
+
+  // create lookup hashes
+  var AtoI = {};
+  var BtoJ = {};
+  var CtoK = {};
+
+  xgroups.forEach(function (xbin, i) {
+    AtoI[xbin.value.toString()] = i;
+  });
+  ygroups.forEach(function (ybin, j) {
+    BtoJ[ybin.value.toString()] = j;
+  });
+  zgroups.forEach(function (zbin, j) {
+    CtoK[zbin.value.toString()] = j;
+  });
+
+    // add data
+  var d = 0;
+  filter.data.forEach(function (group) {
+    if (AtoI.hasOwnProperty(group.a) && BtoJ.hasOwnProperty(group.b) && CtoK.hasOwnProperty(group.c)) {
+      var val = parseInt(group.aa) || 0;
+      if (val > 0) {
+        var i = AtoI[group.a];
+        var j = BtoJ[group.b];
+        var k = CtoK[group.c];
+
+        if (i === +i && j === +j && k === +k) {
+          chartData.x[i] = xgroups.models[i].value;
+          chartData.y[i] = ygroups.models[i].value;
+          chartData.z[i] = zgroups.models[i].value;
+          d++;
+        }
+      }
+    }
+  });
+}
 
 module.exports = AmpersandView.extend({
   template: templates.includes.widgetcontentPlotly,
   renderContent: function () {
     var filter = this.model.filter;
+
+    if (!this._plotly) {
+      initChart(this);
+    }
 
     // redraw when the model indicates new data is available
     filter.on('newData', function () {
@@ -50,55 +83,24 @@ module.exports = AmpersandView.extend({
   },
 
   update: function () {
-    var model = this.model;
+    // var model = this.model;
     var filter = this.model.filter;
 
     this._config = this.model.plotlyConfig();
     var options = this._config.options;
-    var layout = this.model.plotLayout();
+    var layout = this._config.layout;
     var chartData = this._config.data;
     var graphDiv = this.queryByHook('chart-area-plotly');
 
-
-    // //console.log(filter.partitions);
     if (filter.isConfigured && (!this._plotly)) {
       initChart(this);
     }
 
-    var partitionA = filter.partitions.get(1, 'rank');
-    var partitionB = filter.partitions.get(2, 'rank');
-    var partitionC = filter.partitions.get(3, 'rank');
-
-    var xgroups = partitionA.groups;
-    var ygroups = partitionB.groups;
-    var zgroups = partitionC.groups;
-
-
-    xgroups.forEach(function (xbin, i) {
-      chartData.x[i] = xbin.value;
-//      AtoI[xbin.value.toString()] = i;
-    });
-
-    ygroups.forEach(function (ybin, i) {
-      chartData.y[i] = ybin.value;
-//      AtoI[xbin.value.toString()] = i;
-    });
-
-    zgroups.forEach(function (zbin, i) {
-      chartData.z[i] = zbin.value;
-//      AtoI[xbin.value.toString()] = i;
-    });
-
-    this._xgroups = xgroups;
-    this._ygroups = ygroups;
-    this._zgroups = zgroups;
-
-    console.log(chartData);
-    console.log(layout);
-    console.log(options);
+    if (filter.isConfigured) {
+      updateScatter(this);
+    }
 
     // Hand over to Plotly for actual plotting
     this._plotly = Plotly.newPlot(graphDiv, [chartData], layout, options);
-
   }
 });

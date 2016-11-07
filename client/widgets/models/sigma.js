3ea239e941d432a1531a85d0f4a34e4967a46a6e
Jisk Attema <j.attema@esciencecenter.nl>
2016-11-07 10:17:53 +0100

    Network plot improvements
    Closes #30, #33, #35, #36, #37
    For further sytling please open new issue.

--- client/widgets/models/sigma.js
+++ client/widgets/models/sigma.js
@@ -3,7 +3,7 @@ var Chart = require('./chart');
 module.exports = Chart.extend({
   initialize: function () {
     this.minPartitions = 2;
-    this.maxPartitions = 2;
+    this.maxPartitions = 3;
   },
   sigmaConfig: function () {
     return {
--- client/widgets/views/widget-sigma.js
+++ client/widgets/views/widget-sigma.js
@@ -1,74 +1,21 @@
 var AmpersandView = require('ampersand-view');
 var misval = require('../../../framework/util/misval.js');
+var colors = require('../../colors');
 
 // NOTE: sigma and sigma plugins do not work properly with require()
 // workaround via browserify-shim (configured in package.json)
 var Sigma = require('sigmajs');
 require('sigmajs.layout.forceAtlas2');
+require('sigmajs.renderers.parallelEdges');
 
 function deinitChart (view) {
   if (view._sigma) {
-    view._sigma.stopForceAtlas2();
+    view._sigma.killForceAtlas2();
     view._sigma.kill();
     delete view._sigma;
   }
+
   delete view._config;
-}
-
-function drawEdges (view, nodes) {
-  var filter = view.model.filter;
-  var sigma = view._sigma;
-
-  // draw new ones
-  filter.data.forEach(function (group, id) {
-    if (group.a !== misval && group.b !== misval && group.aa !== misval) {
-      if (nodes.hasOwnProperty(group.a) && nodes.hasOwnProperty(group.b)) {
-        sigma.graph.addEdge({
-          id: 'e' + id,
-          source: group.a,
-          target: group.b
-        });
-      }
-    }
-  });
-}
-
-function drawNodes (view) {
-  var filter = view.model.filter;
-  var sigma = view._sigma;
-
-  // a link is between 'src' (partition 1) and 'dst' (partition 2)
-  // so add nodes for all possible start and end points
-  var nodes = {};
-
-  var src = filter.partitions.get(1, 'rank');
-  src.groups.forEach(function (group) {
-    if (!nodes.hasOwnProperty(group.label)) {
-      nodes[group.label] = true;
-      sigma.graph.addNode({
-        id: group.label,
-        label: group.label,
-        size: 1,
-        x: Math.random(),
-        y: Math.random()
-      });
-    }
-  });
-
-  var dst = filter.partitions.get(2, 'rank');
-  dst.groups.forEach(function (group) {
-    if (!nodes.hasOwnProperty(group.label)) {
-      nodes[group.label] = true;
-      sigma.graph.addNode({
-        id: group.label,
-        label: group.label,
-        size: 1,
-        x: Math.random(),
-        y: Math.random()
-      });
-    }
-  });
-  return nodes;
 }
 
 function initChart (view) {
@@ -81,13 +28,94 @@ function initChart (view) {
   // Get a new sigma plot
   view._sigma = new Sigma({
     renderers: [{
-      container: view.el
+      container: view.el,
+      type: 'canvas'
     }],
     settings: view._config
   });
 
   // In callbacks on the chart we will need the view, so store a reference
   view._sigma._Ampersandview = view;
+
+  // cache for nodes and their positions
+  view._nodes = {};
+
+  // number of nodes on screen
+  view._nnodes = 0;
+}
+
+// test if node exits, and add it if not
+function testNode (view, label) {
+  var alpha;
+  var x;
+  var y;
+
+  if (!view._sigma.graph.nodes(label)) {
+    // try to get previous postion, or generate new one
+    if (view._nodes.hasOwnProperty(label)) {
+      x = view._nodes[label].x;
+      y = view._nodes[label].y;
+    } else {
+      alpha = view._nnodes * 2.0 * 3.1415297 / 5.333333;
+      x = 10.0 * Math.cos(alpha);
+      y = 10.0 * Math.sin(alpha);
+    }
+
+    view._sigma.graph.addNode({
+      id: label,
+      label: label,
+      size: 1,
+      color: '#666',
+      x: x,
+      y: y
+    });
+    view._nnodes++;
+  }
+}
+
+function drawGraph (view) {
+  var filter = view.model.filter;
+  var edgeToCount = {};
+  var count;
+  var type;
+
+  var edgePartition = view.model.filter.partitions.get(3, 'rank');
+  if (edgePartition) {
+    type = 'curve';
+    edgePartition.groups.forEach(function (group, n) {
+      edgeToCount[group.value] = n;
+    });
+  } else {
+    count = 0;
+    type = 'line';
+  }
+
+  // draw new ones
+  filter.data.forEach(function (group, id) {
+    if (group.aa !== misval && group.aa !== 0 && group.a !== misval && group.b !== misval) {
+      testNode(view, group.a);
+      testNode(view, group.b);
+
+      if (edgePartition) {
+        if (edgeToCount.hasOwnProperty(group.c)) {
+          count = edgeToCount[group.c];
+        } else {
+          console.log('illegal node / edge');
+          return;
+        }
+      }
+
+      // add edge
+      view._sigma.graph.addEdge({
+        color: colors.getColor(count).css(),
+        id: 'e' + id,
+        source: group.a,
+        target: group.b,
+        count: count,
+        type: type
+      });
+    }
+  });
 }
 
 module.exports = AmpersandView.extend({
@@ -113,18 +141,27 @@ module.exports = AmpersandView.extend({
       initChart(this);
     }
 
-    if (filter.isConfigured) {
-      this._sigma.stopForceAtlas2();
-      this._sigma.graph.clear();
-      var nodes = drawNodes(this);
-      drawEdges(this, nodes);
-      this._sigma.refresh();
-      try {
-        this._sigma.startForceAtlas2({ worker: true, gravity: 1 });
-      } catch (error) {
-        console.log(error);
-      }
-    }
+    this._sigma.killForceAtlas2();
+
+    // remove graph, but cache the node positions
+    this._nodes = {};
+    this._nnodes = 0;
+    this._sigma.graph.nodes().forEach(function (node) {
+      this._nodes[node.id] = node;
+    }, this);
+    this._sigma.graph.clear();
+
+    drawGraph(this);
+    this._sigma.refresh();
+
+    this._sigma.startForceAtlas2({
+      worker: true,
+      adjustSizes: true,
+      barnesHutOptimize: true,
+      edgeWeightInfluence: 1,
+      slowDown: 10,
+      gravity: 1
+    });
   },
 
   initChart: function () {

8f7266f8b2a54fff0375fc0a2423b84f438246f2
Jisk Attema <j.attema@esciencecenter.nl>
2017-01-30 21:44:18 +0100

    Clean up docs

--- framework/util/time.js
+++ framework/util/time.js
@@ -2,7 +2,7 @@ var AmpersandModel = require('ampersand-model');
 var AmpersandColllection = require('ampersand-collection');
 var moment = require('moment-timezone');
 
-/**
+/*
  * Time is grouped by truncating; the resolution is determined in util-time.getResolution()
  * See [this table](http://momentjs.com/docs/#/durations/creating/) for accpetable values
  * when using a crossfilter dataset.

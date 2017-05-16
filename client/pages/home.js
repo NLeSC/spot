5d9a845fa013575b49a712c497d0378c43bebb23
fdiblen <f.diblen@esciencecenter.nl>
2017-05-16 16:03:22 +0200

    Inital version of homepage

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,18 +1,11 @@
 var PageView = require('./base');
 var templates = require('../templates');
 
-var particlesJS = require('particlesjs');
-
 module.exports = PageView.extend({
   template: templates.home,
   autoRender: true,
   initialize: function () {
     this.pageName = 'home';
-
-    particlesJS.init({
-       selector: '.spotparticles'
-     });
-
   },
   pageTitle: 'Home',
   events: {

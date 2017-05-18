538f9983f68352b181a36f1310c29c7a9cdd9561
fdiblen <f.diblen@esciencecenter.nl>
2017-05-18 13:44:56 +0200

    add excel file support. parsed file needs to be formatted still

--- client/pages/datasets.js
+++ client/pages/datasets.js
@@ -3,12 +3,15 @@ var templates = require('../templates');
 var app = require('ampersand-app');
 var csv = require('csv');
 
+var xlsx = require('xlsx');
+
 var ClientDataset = require('../../framework/dataset/client');
 var DatasetCollectionView = require('./datasets/dataset-collection');
 
 module.exports = PageView.extend({
   template: templates.datasets,
   events: {
+    'change [data-hook~=excel-upload-input]': 'uploadEXCEL',
     'change [data-hook~=json-upload-input]': 'uploadJSON',
     'change [data-hook~=csv-upload-input]': 'uploadCSV',
     'click [data-hook~=server-connect]': 'connectServer',
@@ -237,6 +240,121 @@ module.exports = PageView.extend({
     };
 
     reader.readAsText(uploadedFile);
+  },
+  uploadEXCEL: function () {
+    var fileLoader = this.queryByHook('excel-upload-input');
+    var uploadedFile = fileLoader.files[0];
+    var reader = new window.FileReader();
+    var dataURL = fileLoader.files[0].name;
+
+    // enforce client dataset
+    if (this.model.dataview.datasetType !== 'client') {
+      delete this.model.dataview;
+      this.model.dataview = new ClientDataset();
+    }
+
+    var dataset = new ClientDataset({
+      name: dataURL,
+      URL: dataURL,
+      description: 'uploaded EXCEL file'
+    });
+
+    reader.onload = function (ev) {
+      app.message({
+        text: 'Processing',
+        type: 'ok'
+      });
+
+      function fixExceldata (data) {
+        var o = '';
+        var l = 0;
+        var w = 10240;
+        for (; l < data.byteLength / w; ++l) o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)));
+        o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)));
+        return o;
+      }
+
+      function processExcelWorkBook (data, options, callback) {
+        try {
+          var workbook = xlsx.read(btoa(fixExceldata(data)), options);
+          var result = {};
+          workbook.SheetNames.forEach(function (sheetName) {
+            var roa = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+            if (roa.length > 0) {
+              result[sheetName] = roa;
+            }
+          });
+          var output = JSON.stringify(result, 2, 2);
+          if (typeof console !== 'undefined' && options.verbose) {
+            console.log(output);
+          }
+          callback({data: output});
+        } catch (errorMessage) {
+          callback({err: new Error('Excel file processing error: ' + errorMessage)});
+        }
+      }
+
+      var options = {
+        type: 'base64',
+        verbose: false
+      };
+
+      processExcelWorkBook(ev.target.result, options, function (out) {
+        if (out.err) {
+          console.warn(out.err.message);
+          app.message({
+            text: 'EXCEL file parsing problem! Please check the uploaded file',
+            type: 'error',
+            error: ev
+          });
+        } else {
+          dataset.crossfilter.add([out.data]);
+          // automatically analyze dataset
+          dataset.scanData();
+          dataset.facets.forEach(function (facet, i) {
+            if (i < 20) {
+              facet.isActive = true;
+
+              if (facet.isCategorial) {
+                facet.setCategories();
+              } else if (facet.isContinuous || facet.isDatetime || facet.isDuration) {
+                facet.setMinMax();
+              }
+            }
+          });
+          app.message({
+            text: dataURL + ' was uploaded succesfully. Configured ' + dataset.facets.length + ' facets',
+            type: 'ok'
+          });
+          app.me.datasets.add(dataset);
+          window.componentHandler.upgradeDom();
+        }
+      });
+    };
+
+    reader.onerror = function (ev) {
+      console.warn('Error loading EXCEL file.', ev);
+      app.message({
+        text: 'File loading problem!',
+        type: 'error'
+      });
+    };
+
+    reader.onprogress = function (ev) {
+      if (ev.lengthComputable) {
+        // ev.loaded and ev.total are ProgressEvent properties
+        var loaded = (ev.loaded / ev.total);
+        if (loaded < 1) {
+          app.message({
+            text: 'Uploading file ' + (parseInt(loaded * 100)) + '%',
+            type: 'ok'
+          });
+        }
+      }
+    };
+
+    // reader.readAsBinaryString(uploadedFile);
+    reader.readAsArrayBuffer(uploadedFile);
   },
   connectServer: function () {
     app.me.connectToServer(window.location.hostname);

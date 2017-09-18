3ce6a6eac579329ee17f9a023299554b859abbdb
fdiblen <f.diblen@esciencecenter.nl>
2017-09-18 15:41:38 +0200

    use localstorage to welcome only once

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,18 +8,35 @@ module.exports = PageView.extend({
   initialize: function () {
     this.pageName = 'home';
 
-    var intro = Tour.introJs();
-    intro.setOptions({
-                steps: [
-                  {
-                    intro: "Welcome to SPOT!"
-                  },
-                  {
-                    intro: "You <b>don't need</b> to define element to focus, this is a floating tooltip."
-                  }
-                ]
-              });
-    intro.start();
+    var introWelcome = Tour.introJs();
+    introWelcome.setOptions({
+      steps: [
+        {
+          intro: 'Welcome to SPOT!'
+        },
+        {
+          intro: "You <b>don't need</b> to define element to focus, this is a floating tooltip."
+        }
+      ]
+    });
+
+    // add a flag when we're done
+    introWelcome.oncomplete(function () {
+      localStorage.setItem('doneWelcome', 'great!');
+    });
+
+    // add a flag when we exit
+    introWelcome.onexit(function () {
+      localStorage.setItem('doneWelcome', 'great!');
+    });
+
+    var doneWelcome = localStorage.getItem('doneWelcome') === 'great!';
+    if (doneWelcome) {
+      console.log('Did the tour already!');
+    } else {
+      console.log('Starting the tour.');
+      introWelcome.start();
+    }
   },
   pageTitle: 'Home',
   template: templates.home,

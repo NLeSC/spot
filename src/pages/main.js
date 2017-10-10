191c4132054392996b8c135d82e98f3d1e76cc43
fdiblen <f.diblen@esciencecenter.nl>
2017-10-10 11:49:34 +0200

    remove color picker from the drawer

--- src/pages/main.js
+++ src/pages/main.js
@@ -10,7 +10,7 @@ var templates = require('../templates');
 
 // For the help
 var Tour = require('intro.js');
-var ColorPicker = require('simple-color-picker');
+// var ColorPicker = require('simple-color-picker');
 
 function checkConnection (model) {
   if (model.sessionType === 'server' && !model.isConnected) {
@@ -49,23 +49,7 @@ module.exports = View.extend({
   events: {
     'click a[href]': 'handleLinkClick',
     'click [data-hook~=tour-button]': 'startTour',
-    'click [data-hook~=menu-button]': 'handleMenu',
-
-    'click [data-hook~=menu-color-button]': 'showColorSettings',
-    'click [data-hook~=menu-color-original]': function () { this.colorpicker.setColor('#223446'); },
-    'click [data-hook~=color-settings-close]': 'closeColorSettings'
-  },
-  setColorPicker: function () {
-    var picker = this.queryByHook('color-palette');
-    var colorPicker = new ColorPicker({
-      color: '#223446',
-      background: '#454545',
-      el: picker,
-      width: 200,
-      height: 200
-    });
-
-    return colorPicker;
+    'click [data-hook~=menu-button]': 'handleMenu'
   },
   startTour: function () {
     var intro = Tour.introJs();
@@ -75,44 +59,14 @@ module.exports = View.extend({
     var drawer = this.queryByHook('main-drawer');
     drawer.classList.toggle('is-expanded');
   },
-  showColorSettings: function () {
-    var dialog = this.queryByHook('color-settings');
-    dialog.showModal();
-
-    if (typeof this.colorpicker === 'undefined') {
-      this.colorpicker = this.setColorPicker();
-    }
-
-    var picker = this.queryByHook('color-palette');
-    var drawer = this.queryByHook('main-drawer');
-    var navMenu = this.queryByHook('nav-menu');
-    var menuSpacer = this.queryByHook('menu-spacer');
-    var colorDialog = this.queryByHook('color-settings');
-    var pageContainer = this.queryByHook('page-container');
-
-    this.colorpicker.onChange(function (hexStringColor) {
-      picker.style.background = hexStringColor;
-      drawer.style.background = hexStringColor;
-      menuSpacer.style.background = hexStringColor;
-      navMenu.style.background = hexStringColor;
-      colorDialog.style.background = hexStringColor;
-      pageContainer.style.background = hexStringColor;
-    });
-  },
-  closeColorSettings: function () {
-    var dialog = this.queryByHook('color-settings');
-    dialog.close();
-  },
   changeMenuColor: function (color) {
     var drawer = this.queryByHook('main-drawer');
     var navMenu = this.queryByHook('nav-menu');
     var menuSpacer = this.queryByHook('menu-spacer');
-    var colorDialog = this.queryByHook('color-settings');
     var pageContainer = this.queryByHook('page-container');
     drawer.style.background = color;
     menuSpacer.style.background = color;
     navMenu.style.background = color;
-    colorDialog.style.background = color;
     pageContainer.style.background = color;
   },
   expandMenu: function () {

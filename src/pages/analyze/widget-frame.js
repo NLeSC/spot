f5995ef74640d1ad41fb82afba3ee64e59a38a6e
fdiblen <f.diblen@esciencecenter.nl>
2017-10-16 23:50:14 +0200

    add chart export option

--- src/pages/analyze/widget-frame.js
+++ src/pages/analyze/widget-frame.js
@@ -3,6 +3,7 @@ var templates = require('../../templates');
 var app = require('ampersand-app');
 var SlotView = require('./slot');
 var $ = require('jquery');
+var FileSaver = require('file-saver');
 
 // NOTE: gridster does not work properly with require()
 // workaround via browserify-shim (configured in package.json)
@@ -79,6 +80,7 @@ module.exports = View.extend({
     'click [data-hook~="close"]': 'closeWidget',
     'click [data-hook~="zoom-in"]': 'zoomIn',
     'click [data-hook~="zoom-out"]': 'zoomOut',
+    'click [data-hook~="save"]': 'savePlot',
     'click [data-hook~="edit"]': function () { this.editMode = !this.editMode; },
 
     'mouseenter .widgetFrame': 'mouseEnter',
@@ -91,6 +93,31 @@ module.exports = View.extend({
   zoomOut: function () {
     this.model.filter.zoomOut();
     app.me.dataview.getData();
+  },
+  savePlot: function () {
+    var transparent = true;
+
+    var canvas = this.queryByHook('canvas');
+    var ctx = canvas.getContext('2d');
+    var imageName = this.model.modelType;
+
+    if (transparent) {
+      canvas.toBlob(function (blob) {
+        FileSaver.saveAs(blob, imageName);
+      }, 'image/png');
+    } else {
+      var tempCanvas = document.createElement('canvas');
+      var tempCtx = tempCanvas.getContext('2d');
+      tempCanvas.width = ctx.canvas.width;
+      tempCanvas.height = ctx.canvas.height;
+      tempCtx.fillStyle = 'white';
+      tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
+      tempCtx.drawImage(canvas, 0, 0);
+
+      tempCtx.canvas.toBlob(function (blob) {
+        FileSaver.saveAs(blob, imageName);
+      }, 'image/png');
+    }
   },
   mouseEnter: function () {
     this.mouseOver = true;
